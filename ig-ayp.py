98d8b0b16632164bfbff05f94436c7a1706dbb5f
Jona <jonathancardonarojas@proton.me>
2023-09-22 04:12:32 -0500

    First draft for full code to be developed

---
+++ ig-ayp.py
@@ -0,0 +1,26 @@
+import sys,os
+
+# intro
+print("Instagram At Your Pace - IG-AYP")
+print()
+print("Schedule downloading of instagram stories to view them at your own pace, in your own schedule.")
+print("You need to setup an Instagram user account to be able to use it")
+
+while True:
+    # if config does not exist
+    print("Press 'c' to configure")
+    # else break
+
+def downloadStories():
+    # instagram-story code
+    
+print("This utility uses instagram-story module to download instagram stories")
+print()
+print("You can schedule the download to be done at different frequencies or use it just once")
+print("Press any key to use it once or 's' to select a repeat frequency")
+freq=input(string())
+if freq==s:
+    # Schedule code
+else:
+    # One time execution
+print("")
